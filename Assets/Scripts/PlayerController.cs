1bc3c47e1e3fb9573e561cff7c440c939935f6f1
Daniele Piergigli <daniele.piergigli@gmail.com>
2017-11-04 20:27:01 +0100

    Added player movement on Z axe

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,9 @@ public class PlayerController : MonoBehaviour {
     public float speed = 10f;
 
     Animator animator;
-    // At the start the player facing right
+    // At the start the player facing right and bottom
     bool facingRight = true;
+    bool facingTop;
 
 	// Use this for initialization
 	void Start () {
@@ -18,23 +19,51 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontal = Input.GetAxis("Horizontal");
-        // start the movement animation
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
-        // move the player on the x axes
-        transform.Translate(horizontal * speed * Time.deltaTime, 0f, 0f);
-        // with a certain direction
-        Flip(horizontal);
+        Move();
 	}
 
-    // flip the movement direction
-    void Flip(float horizontal) {
+    // move the player
+    void Move() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // if player is moving left or right
+        if (Mathf.RoundToInt(horizontal) != 0) {
+            // start the movement animation
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+            // move the player on the x axes
+            transform.Translate(horizontal * speed * Time.deltaTime, 0f, 0f);
+            // with a certain direction of the sprite
+            FlipX(horizontal);
+        }
+        else {
+            // start the movement animation
+            animator.SetFloat("Speed", Mathf.Abs(vertical));
+            // move the player on the x axes
+            transform.Translate(0f, 0f, vertical * speed * Time.deltaTime);
+            // with a certain direction of the sprite
+            FlipY(vertical);
+        }
+    }
+
+    // flip the movement direction on horizontal axe
+    void FlipX(float horizontal) {
         // if the movement od the player is in the opposite direction compered to where the character looks
         if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight)) {
             facingRight = !facingRight;
             // mirror the sprite renderer
             SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.flipX = !facingRight;
+        }
+    }
+
+    // flip the movement direction on vertical axe
+    void FlipY(float vertical) {
+        // if the movement od the player is in the opposite direction compered to where the character looks
+        if ((vertical > 0 && !facingTop) || (vertical < 0 && facingTop)) {
+            facingTop = !facingTop;
+            // mirror the sprite renderer
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipY = !facingTop;
         }
     }
 }
