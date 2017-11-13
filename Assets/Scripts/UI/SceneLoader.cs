fa256911c51508381ab2ce6d8f92da6db63b6988
Maharez <Simone.brunitti@gmail.com>
2017-11-13 19:07:58 +0100

    Added main menu and level select menu

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public int level;
+
+	public void SwitchScene() {
+        SceneManager.LoadScene(level);
+    }
+
+    public void SetScene(int level)
+    {
+        this.level = level;
+    }
+
+}
---
+++ Assets/Scripts/UI/SceneSelectionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneSelectionController : MonoBehaviour {
+
+    public GameObject startButtonGameObject;
+    private Button startButton;
+    private SceneLoader sceneLoader;
+
+    public void Start()
+    {
+        sceneLoader = startButtonGameObject.GetComponent<SceneLoader>();
+        startButton = startButtonGameObject.GetComponent<Button>();
+    }
+
+    public void SwitchScene(int level)
+    {
+        sceneLoader.SetScene(level);
+        if(!startButton.IsInteractable())
+            startButton.interactable = true;
+    }
+}
