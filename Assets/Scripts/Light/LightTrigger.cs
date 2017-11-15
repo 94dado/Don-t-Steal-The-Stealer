166ca93e0fcd65f89ae713ee9fda1ee79ad59241
Davide Quadrelli <ildado94@gmail.com>
2017-11-15 18:21:28 +0100

    Updated lights prefab, added light trigger script

---
+++ Assets/Scripts/Light/LightTrigger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class LightTrigger : MonoBehaviour {
+
+	//turn on/off the lightning
+    void SetLightining(bool value) {
+        GetComponentInChildren<Light>().enabled = value;
+    }
+}
