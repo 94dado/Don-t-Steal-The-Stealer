e7e42780df79ec149a26be04b3dc82b799bfcf6f
Maharez <Simone.brunitti@gmail.com>
2017-11-15 16:47:01 +0100

    Player interaction test level

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour {
 
     // speed of the player
     public float speed;
+    public Transform lineStart,lineEnd, lineEndWest,lineEndEast,lineEndSouth,LineEndNorth;
+    public GameObject interactionText;
+    RaycastHit2D hitObject;
+    bool interact = false;
 
     Animator animator;
     // where the last movemement is
@@ -23,6 +27,7 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Move();
+        Raycast();
     }
 
     // move the player
@@ -37,12 +42,20 @@ public class PlayerController : MonoBehaviour {
             transform.Translate(horizontal * currentSpeed * Time.deltaTime, 0f, 0f);
             isRunning = true;
             lastMovement = new Vector2(horizontal, 0f);
+            if (horizontal > 0f)
+                lineEnd = lineEndEast;
+            else
+                lineEnd = lineEndWest;
         }
         // move vertically
         if (vertical > 0f || vertical < 0f) {
             transform.Translate(0f, vertical * currentSpeed * Time.deltaTime, 0f);
             isRunning = true;
             lastMovement = new Vector2(0f, vertical);
+            if (vertical > 0f)
+                lineEnd = LineEndNorth;
+            else
+                lineEnd = lineEndSouth;
         }
         // check if we move in diagonal on the map and it reduces the speed
         if (Mathf.Abs(horizontal) > 0.5f && Mathf.Abs(vertical) > 0.5f) {
@@ -58,4 +71,24 @@ public class PlayerController : MonoBehaviour {
         animator.SetFloat("LastRunY", lastMovement.y);
         animator.SetBool("Running", isRunning);
     }
+
+    void Raycast() {
+        Debug.DrawLine(lineStart.position, lineEnd.position, Color.black);
+
+        if (Physics2D.Linecast(lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer("Interactable")))
+        {
+            hitObject = Physics2D.Linecast(lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer("Interactable"));
+            interact = true;
+            interactionText.SetActive(true);
+        }
+        else
+        {
+            interact = false;
+            interactionText.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && interact == true)
+            hitObject.collider.gameObject.GetComponent<InteractableObject>().Interact();
+
+    }
+
 }
