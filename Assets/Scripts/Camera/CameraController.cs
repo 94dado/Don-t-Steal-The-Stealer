29b3463a418938e985a0dfd38391acacafedd761
Daniele Piergigli <daniele.piergigli@gmail.com>
2017-11-27 17:49:05 +0100

    Added camera with boundaries script

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,11 +1,35 @@
 ﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    
+
+    // player
     public Transform target;
+    // bound diagonal positions
+    public Transform maxBound;
+    public Transform minBound;
+
+    Camera cam;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
 
     void Update() {
+        // get the camera's aspect ratio
+        float camVertExtent = cam.orthographicSize;
+        float camHorzExtent = cam.aspect * camVertExtent;
+
+        // get bounds
+        float leftBound = minBound.position.x + camHorzExtent;
+        float rightBound = maxBound.position.x - camHorzExtent;
+        float bottomBound = minBound.position.y + camVertExtent;
+        float topBound = maxBound.position.y - camVertExtent;
+
+        // clamp camera to borders
+        float camX = Mathf.Clamp(target.position.x, leftBound, rightBound);
+        float camY = Mathf.Clamp(target.position.y, bottomBound, topBound);
+
         // move camera
-        transform.position = target.position;
+        cam.transform.position = new Vector3(camX, camY, cam.transform.position.z);
     }
 }
