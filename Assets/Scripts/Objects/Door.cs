a14307a9f5f438f9c1e5e485ad5d852f09609a94
Maharez <Simone.brunitti@gmail.com>
2017-12-06 13:25:28 +0100

    Doors now need keys to be opened

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : InteractableObject {
+
+    public int doorID;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    override
+    public int Interact()
+    {
+        animator.SetBool("interacted", true);
+        Collider2D[] colliders;
+        colliders = this.gameObject.GetComponents<Collider2D>() as Collider2D[];
+        colliders[0].enabled = false;
+        colliders[1].enabled = true;
+        gameObject.layer = LayerMask.NameToLayer("Interacted");
+
+        return value;
+    }
+
+    public int getDoorID()
+    {
+        return this.doorID;
+    }
+}
