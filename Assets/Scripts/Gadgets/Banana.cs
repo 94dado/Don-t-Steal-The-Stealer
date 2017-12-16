ef20ca38fc5f35bc70a3a2de99cee26a9582c290
Maharez <Simone.brunitti@gmail.com>
2017-12-16 18:19:55 +0100

    Added Banana script

---
+++ Assets/Scripts/Gadgets/Banana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : Gadgets {
+
+    public Banana(string name, Sprite sprite, float cooldown, float boostDuration, PlayerController player) : base(name, sprite, cooldown, boostDuration, player)
+    {
+
+    }
+
+    override
+    public void activateGadget()
+    {
+        if (cooldownTimer == 0)
+        {
+            boostDurationTimer = boostDuration;
+            cooldownTimer = cooldown;
+        }
+    }
+}
