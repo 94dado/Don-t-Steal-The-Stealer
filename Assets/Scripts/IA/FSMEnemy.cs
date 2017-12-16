32cc3d17174e838247df80ca0919b7f5d46817df
Davide Quadrelli <ildado94@gmail.com>
2017-12-16 18:01:25 +0100

    Fixed AI and added new sprites and prefab

--- Assets/Scripts/IA/FSMEnemy.cs
+++ Assets/Scripts/IA/FSMEnemy.cs
@@ -84,7 +84,8 @@ public class FSMEnemy : SpriteOffset {
     // moves the player
     void Moving() {
         // move player to next position until it will reach it
-        if (Vector2.Distance(transform.position, nextPosition.position) > 0f) {
+        Debug.Log(Vector2.Distance(transform.position, nextPosition.position));
+        if (Vector2.Distance(transform.position, nextPosition.position) > 0.01f) {
             // move
             transform.position = Vector2.MoveTowards(transform.position, nextPosition.position, currentSpeed * Time.deltaTime);
         }
