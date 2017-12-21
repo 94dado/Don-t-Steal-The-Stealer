b165038792a224ed8950d28c4d4546506835a9f0
Davide Quadrelli <ildado94@gmail.com>
2017-12-21 15:55:21 +0100

    Created prefabs for assets
    Now every asset has a prefab

---
+++ Assets/Scripts/Objects/DisappearingObject.cs
@@ -0,0 +1,12 @@
+﻿public class DisappearingObject : InteractableObject {
+
+    //defined empty to NOT execute the father start that is not needed
+    public void Start() {
+
+    }
+
+    public override int Interact() {
+        Destroy(gameObject);
+        return value;
+    }
+}
