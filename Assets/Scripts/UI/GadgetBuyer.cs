b6b9afbd21dbaa35e2b4ce6a4ee941c67a8ee6cc
Maharez <Simone.brunitti@gmail.com>
2017-12-29 19:54:53 +0100

    Implemented a gadget menu

---
+++ Assets/Scripts/UI/GadgetBuyer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GadgetBuyer : MonoBehaviour {
+
+    public  int gadgetID;
+    private DataManager dataManager;
+    public Text description;
+
+    private void Start()
+    {
+        dataManager = GameObject.FindGameObjectWithTag("DataManager").GetComponent<DataManager>();
+    }
+
+    public void buyGadget()
+    {
+        if(dataManager.MoneyData >= dataManager.Gadgets[gadgetID].price)
+        {
+            dataManager.MoneyData = dataManager.MoneyData - dataManager.Gadgets[gadgetID].price;
+            dataManager.Gadgets[gadgetID].isLocked = false;
+        }
+    }
+
+    public void setGadget(int gadgetID)
+    {
+        this.gadgetID = gadgetID;
+        
+    }
+
+    public int getGadget()
+    {
+        return this.gadgetID;
+    }
+}
