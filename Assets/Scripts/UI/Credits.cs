d11c190d1f1666cda95decd161b92c6510286fb8
Davide Quadrelli <ildado94@gmail.com>
2018-01-01 18:07:39 +0100

    Finished credits scene

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class Credits : MonoBehaviour {
+    public Button backButton;
+    Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Close")) {
+            backButton.GetComponent<SceneLoader>().SwitchScene();
+        }
+	}
+}
