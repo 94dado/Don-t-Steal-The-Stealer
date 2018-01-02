31c2d809f079a3d3a7e0295da2560e52fbb22b30
Davide Quadrelli <ildado94@gmail.com>
2018-01-02 15:40:12 +0100

    Fixed scene changing in every scene, added intro
    Added intro when starting new game. Fixed scenes changing using scene name and not their index.

--- Assets/Scripts/UI/Credits.cs
+++ Assets/Scripts/UI/Credits.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 public class Credits : MonoBehaviour {
     public Button backButton;
     Animator animator;
+    SceneLoader sceneLoader;
 
     void Start() {
         animator = GetComponent<Animator>();
+        if(backButton != null) {
+            sceneLoader = backButton.GetComponent<SceneLoader>();
+        }
+        else {
+            sceneLoader = GetComponent<SceneLoader>();
+        }
     }
 
     // Update is called once per frame
     void Update () {
         if (animator.GetCurrentAnimatorStateInfo(0).IsName("Close")) {
-            backButton.GetComponent<SceneLoader>().SwitchScene();
+            sceneLoader.SwitchScene();
         }
 	}
 }
