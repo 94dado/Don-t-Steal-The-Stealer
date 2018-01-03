b9634fa5408c4d05b6baf9fa9c75f890875e8af4
Maharez <Simone.brunitti@gmail.com>
2018-01-03 15:54:58 +0100

    Addedlaser behaviour

---
+++ Assets/Scripts/Objects/Laser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameManager.instance; 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            gameManager.gameOver = true;
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
