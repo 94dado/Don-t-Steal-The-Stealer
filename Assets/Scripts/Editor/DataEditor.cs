b0ab88463ca9299533a41b0c039256b75689c962
Maharez <Simone.brunitti@gmail.com>
2018-01-05 15:35:56 +0100

    Fixed many bugs

--- Assets/Scripts/Editor/DataEditor.cs
+++ Assets/Scripts/Editor/DataEditor.cs
@@ -8,5 +8,28 @@ public class DataEditor {
 		string dataPath = Path.Combine(Application.persistentDataPath,"data.json");
         Debug.Log(dataPath);
 		File.Delete(dataPath);
-	}
+        PlayerPrefs.SetInt("GameStarted", 0);
+        DataManager dataManager = GameObject.FindGameObjectWithTag("DataManager").GetComponent<DataManager>();
+
+        
+        foreach (Gadget g in dataManager.Gadgets)
+        {
+            g.isLocked = true;
+        }
+
+        foreach (Level l in dataManager.Levels)
+        {
+            l.isLocked = true;
+            l.levelCompleted = false;
+            l.objectsScore = 0;
+            l.timeScore = 0;
+
+        }
+
+        foreach (Intelligence i in dataManager.Intelligence)
+        {
+            i.isLocked = true;
+        }
+
+    }
 }
--- Assets/Scripts/UI/GadgetBuyer.cs
+++ Assets/Scripts/UI/GadgetBuyer.cs
@@ -8,20 +8,29 @@ public class GadgetBuyer : MonoBehaviour {
     public  int gadgetID;
     private DataManager dataManager;
     public Text description;
+    private Button buyButton;
 
     private void Start()
     {
         dataManager = GameObject.FindGameObjectWithTag("DataManager").GetComponent<DataManager>();
+        buyButton = GetComponent<Button>();
+        gadgetID = -1;
+    }
+
+    private void Update()
+    {
+        if (gadgetID != -1 && buyButton.interactable == false)
+            buyButton.interactable = true;
     }
 
     public void buyGadget()
     {
-        if(gadgetID < dataManager.Gadgets.Length && dataManager.MoneyData >= dataManager.Gadgets[gadgetID].price)
+        if(gadgetID < dataManager.Gadgets.Length && dataManager.Gadgets[gadgetID].isLocked && dataManager.MoneyData >= dataManager.Gadgets[gadgetID].price)
         {
             dataManager.MoneyData = dataManager.MoneyData - dataManager.Gadgets[gadgetID].price;
             dataManager.Gadgets[gadgetID].isLocked = false;
         }
-        else if (dataManager.MoneyData >= dataManager.Intelligence[gadgetID - dataManager.Gadgets.Length].price && gadgetID >= dataManager.Gadgets.Length)
+        else if (gadgetID >= dataManager.Gadgets.Length && dataManager.Intelligence[gadgetID - dataManager.Gadgets.Length].isLocked && dataManager.MoneyData >= dataManager.Intelligence[gadgetID - dataManager.Gadgets.Length].price)
         {
             dataManager.MoneyData = dataManager.MoneyData - dataManager.Intelligence[gadgetID - dataManager.Gadgets.Length].price;
             dataManager.Intelligence[gadgetID - dataManager.Gadgets.Length].isLocked = false;
